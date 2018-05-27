b92be784c6d6edc3845ee527351b95c5ab5f79cd
TomaszWloszkiewicz <tomek.wloszkiewicz@gmail.com>
2018-05-27 02:06:12 +0200

    Movie-microservice

--- src/main/java/com/movie/MoviesManagerLauncher.java
+++ src/main/java/com/movie/MoviesManagerLauncher.java
@@ -1,9 +1,7 @@
-package com.manager;
+package com.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
